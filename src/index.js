494e051f29aa56dbf5046aa288482685c2e6be1b
Iram Garcia <iramlgarcia@gmail.com>
2018-06-17 23:37:15 -0300

    upgrading to sass

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
 import App from "./App";
-import style from "./main.css";
+import style from "./base.scss";
--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,21 @@ module.exports = {
         ]
       },
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"]
+        test: /\.scss$/,
+        use: [
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[ext]",
+          },
+        },
       }
     ]
   },
